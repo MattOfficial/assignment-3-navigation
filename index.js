6ef959dc2ec19eda4cbf4f8f7c2fc1b70a29e5eb
MattOfficial <chand.matruprasad@outlook.com>
2022-08-11 12:59:21 +0530

    Assignment complete

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const routes = require('./routes/routes');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
